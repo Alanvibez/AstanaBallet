dad1b5a274c9e24773c14846c3c4a72d844739f4
Alikhan <alihan9009@gmail.com>
2022-12-15 14:06:42 +0300

    add audio and favicon

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,24 +1,36 @@
 AOS.init()
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     $('.slider__body').slick({
         infinite: false,
         speed: 1000,
         slidesToShow: 1,
         variableWidth: false,
-        arrows:false,
-        autoplaySpeed:3000,
-        pauseOnHover:true,
+        arrows: false,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
     });
-    $('.slider__body').on('afterChange', function() {
+    $('.slider__body').on('afterChange', function () {
         // Hide/show captions
-        if(!$('video').hasClass('slick-active')){
+        if (!$('video').hasClass('slick-active')) {
             $('video').trigger('pause')
         }
     });
+
+    $('audio').prop('volume', 0.4)
+    $('video').on('play', function () {
+        $('audio').trigger('pause')
+    })
+
+    $('video').on('pause', function () {
+        $('audio').trigger('play')
+    })
+
+    window.addEventListener('focus', function () {
+        $('audio').trigger('play')
+    });
+
+    window.addEventListener('blur', function () {
+        $('audio').trigger('pause')
+    });
 });
-
-
-
-
-
